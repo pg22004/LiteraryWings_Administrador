b4b4921a42c5cb4d453f99761991add3f4ceb48e
SERRANOGRIS <ss22001@esfe.agape.edu.sv>
2023-10-12 14:50:32 -0300

    Codificacion de Editoriales

--- LiteraryWings_Administrador/Models/Editorial.cs
+++ LiteraryWings_Administrador/Models/Editorial.cs
@@ -1,6 +1,14 @@
-﻿namespace LiteraryWings_Administrador.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiteraryWings_Administrador.Models
 {
     public class Editorial
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
     }
 }
+
