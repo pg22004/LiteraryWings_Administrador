aa572fca92d7e93aea12491365aa237069019b6d
NestorEstupinian <em22001@esfe.edu.agape.edu.sv>
2023-10-12 13:39:19 -0600

    Codificación de models de rol y autor

--- LiteraryWings_Administrador/Models/Autor.cs
+++ LiteraryWings_Administrador/Models/Autor.cs
@@ -1,6 +1,27 @@
-﻿namespace LiteraryWings_Administrador.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiteraryWings_Administrador.Models
 {
     public class Autor
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es requerida")]
+        public string FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "La  imagen es requerida")]
+        public string AutorImagen { get; set; }
+
+        [Required(ErrorMessage = "El seudonimo es requerido")]
+        public string Seudonimo { get; set; }
+
+        [Required(ErrorMessage = "La nacionalidad es requerida")]
+        public string Nacionalidad { get; set; }
     }
 }
