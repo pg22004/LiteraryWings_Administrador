ca7b59a4b36a1cc0eb3dc96e1f0340464824e234
Iliana <pg22004@esfe.agape.edu.sv>
2023-10-15 21:57:39 +0200

    Codificacion de paginas de Usuario y Categoria

--- LiteraryWings_Administrador/Models/Categoria.cs
+++ LiteraryWings_Administrador/Models/Categoria.cs
@@ -1,6 +1,12 @@
-﻿namespace LiteraryWings_Administrador.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiteraryWings_Administrador.Models
 {
     public class Categoria
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
     }
 }
